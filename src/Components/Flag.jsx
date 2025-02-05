218bee3289a9436bffee1d1ab8b36e76189d0d8a
Nishit Rana <nishitrana@Nishits-MacBook-Air.local>
2025-02-05 21:04:02 +0530

    second commit

--- src/Components/Flag.jsx
+++ src/Components/Flag.jsx
@@ -39,10 +39,15 @@ export default function FlagsCard(){
 
     useEffect(()=>{
         const apifetch = async () => { 
+            try{
 
+            
             const response = await fetch("https://xcountries-backend.azurewebsites.net/all");
             const data =  await response.json();
-            setFlag(data);  
+            setFlag(data);
+        }catch(e){
+            console.error("The error is:",e);
+        } 
         }
         apifetch();    
     },[])
