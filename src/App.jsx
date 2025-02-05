c0b6f7190cf4c5971bcfc9babf305b674b3327b8
Nishit Rana <nishitrana@Nishits-MacBook-Air.local>
2025-02-05 20:54:15 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import './App.css'
+import FlagsCard from './Components/Flag'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <FlagsCard /> 
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Flag.jsx
@@ -0,0 +1,62 @@
+// import React from "react"
+import { useEffect, useState } from "react";
+import "./Flag.css";
+
+// eslint-disable-next-line react/prop-types
+const CountryCard = ({name,flag}) => {
+
+    return(
+        <div style={{
+            height:"200px",
+            width:"200px",
+            display:"flex",
+            justifyContent:"center",
+            alignItems:"center",
+            flexDirection:"column",
+            margin:"10px",
+            border:"solid",
+            borderRadius:"5%",
+            textAlign:"center"
+            }}>
+        <img style={{
+            height:"80px",
+             width:"80px",
+             display:"flex",
+             alignItems:"center",
+
+             }} src={flag} alt={`Flag of ${name}`} />   
+        <h2>{name}</h2>
+        </div>
+    
+    )
+
+
+}
+
+export default function FlagsCard(){
+
+    const [Flag,setFlag] = useState([]);
+
+    useEffect(()=>{
+        const apifetch = async () => { 
+
+            const response = await fetch("https://xcountries-backend.azurewebsites.net/all");
+            const data =  await response.json();
+            setFlag(data);  
+        }
+        apifetch();    
+    },[])
+    
+    return (
+        <div style={{
+            display:"flex",
+            justifyContent:"center",
+            flexWrap:"wrap",
+        }} className="flags-container">
+          {Flag.map((country) => (
+            <CountryCard key={country.abbr} name={country.name} flag={country.flag} />
+          ))}
+        </div>
+      );
+    
+}
